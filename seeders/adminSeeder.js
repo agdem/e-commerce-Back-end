b198f91bfc9fce01733841b362915328958ffc71
agdem <104213214+agdem@users.noreply.github.com>
2022-07-01 10:19:28 -0300

    admin seeder

--- seeders/adminSeeder.js
+++ seeders/adminSeeder.js
@@ -3,20 +3,12 @@ const { Admin } = require("../models");
 module.exports = async () => {
   const admins = [];
 
-  admins.push(
-    {
-      firstName: "Maria",
-      lastName: "Perez",
-      email: "maria@gmail.com",
-      password: "123",
-    },
-    {
-      firstName: "Jose",
-      lastName: "Garcia",
-      email: "jose@gmail.com",
-      password: "123",
-    }
-  );
+  admins.push({
+    firstName: "Admin",
+    lastName: "Admin",
+    email: "admin@admin.com",
+    password: "1234",
+  });
 
   await Admin.bulkCreate(admins);
   console.log("[Database] Se corrió el seeder de Admins.");
