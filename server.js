d7e1cd482bb7aec6485aa4ae0e4f1b6cbae73cbc
agdem <104213214+agdem@users.noreply.github.com>
2022-07-04 16:37:57 -0300

    corse

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const routes = require("./routes");
 const dbInitialSetup = require("./dbInitialSetup");
 const APP_PORT = process.env.APP_PORT || 8000;
 const app = express();
+app.use(cors());
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
 
 routes(app);
 
